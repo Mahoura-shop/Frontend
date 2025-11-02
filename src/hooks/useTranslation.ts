2bb912f3ab3a1dbd10cd362e50c05df6eb15a8b4
AminFiroozi <afiroozi007@gmail.com>
2025-11-03 01:41:10 +0330

    fix: translation errors

--- src/hooks/useTranslation.ts
+++ src/hooks/useTranslation.ts
@@ -1,12 +1,12 @@
-import { useUIStore } from '../lib/store';
-import { translations, TranslationKey } from '../lib/translations';
+import { useUIStore } from "../store/store";
+import { translations, TranslationKey } from "../utils/translations";
 
 export function useTranslation() {
-  const language = useUIStore((state) => state.language);
+	const language = useUIStore((state) => state.language);
 
-  const t = (key: TranslationKey): string => {
-    return translations[language][key];
-  };
+	const t = (key: TranslationKey): string => {
+		return translations[language][key];
+	};
 
-  return { t, language };
+	return { t, language };
 }
